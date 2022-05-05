6668399e452e962d937f5a4cb56c992aa6c1d888
1997tyagi <103039292+1997tyagi@users.noreply.github.com>
2022-05-05 17:56:29 +0530

    index file commit

---
+++ about.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>learn git</title>
+</head>
+<body>
+    <h1>Learn Git</h1>
+    <?php
+        for ($x = 0; $x <= 10; $x++) {
+        echo "The number is: $x <br>";
+        echo "The number is: $y <br>";
+
+        }
+    ?>
+</body>
+</html>
